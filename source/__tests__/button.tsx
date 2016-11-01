bc36d01704a0ed49640030305ffe262fd4931300
Adam Walker <adam@rocketmakers.com>
2016-11-01 12:03:37 +0000

    started adding some tests

---
+++ source/__tests__/button.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { shallow } from 'enzyme';
+import { Button } from './../components/interaction/button';
+
+it('Button contains a single child when set', () => {
+  const button = shallow(
+    <Button>waddup</Button>
+  );
+  expect(button.children().length).toEqual(1);
+});
+
+it('Button fires an event when clicked', () => {
+  let output;
+  const button = shallow(
+    <Button onClick={()=> output = "hello world"}>waddup</Button>
+  );
+
+  button.find('button').simulate('click');
+  expect(output).toEqual("hello world")
+});
